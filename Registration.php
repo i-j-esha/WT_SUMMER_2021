0db3671a311d6d95e7ba5e14140fcd831f34eb22
i-j-esha <esha.ishrat1810@gmail.com>
2021-05-31 21:23:58 +0600

    first commit

---
+++ Registration.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+
+<h1>Registration Form</h1> 
+<hr>
+<form>
+<table >
+<tr>
+<td>Full Name:</td> 
+<td><input type="text" name="fname"></td>
+</tr>
+<tr>
+<td>Email:</td> 
+<td><input type="email"></td>
+</tr>
+<tr>
+<td>Password:</td> 
+<td><input type="password"></td>
+</tr>
+<tr>
+<td>Comment:</td> 
+<td><textarea rows="5" cols="40" name="comment"></textarea></td>
+</tr>
+<tr>
+<td>Gender:</td>
+<td>
+<input type="radio" name="gender">Male
+<input type="radio" name="gender">Female
+<input type="radio" name="gender">Other
+</td>
+</tr>
+<tr>
+<td>Please choose a Hobby</td> 
+<td>
+<input type="checkbox" name="hobby1">Singing
+<input type="checkbox" name="hobby2">Dancing 
+<input type="checkbox" name="hobby3">Reading 
+</td>
+</tr>
+<tr>
+<td>Please Choose a File</td> 
+<td>
+<input type="file" id="myfile" name="myfile">
+</td>
+</tr>
+</table>
+
+<input type="submit" value="submit">
+<input type="reset">
+
+
+</form>
+</body>
+</html>
+
+
+
+
+
+
